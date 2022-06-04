731034bc11aba3f6b61d23ac47a5a6106ab096c0
tableonthewall <seydaozdemir07@gmail.com>
2022-06-04 20:06:24 +0300

    create gui method added MultiPlayerGame interface

--- src/main/java/A_Giris/game/MultiPlayerGame.java
+++ src/main/java/A_Giris/game/MultiPlayerGame.java
@@ -1,4 +1,10 @@
 package A_Giris.game;
 
+import A_Giris.model.WordlPuzzle;
+
+import javax.swing.*;
+import java.io.IOException;
+
 public interface MultiPlayerGame {
+    void createAndShowGui(WordlPuzzle puzzle, JFrame jFrame) throws IOException;
 }
