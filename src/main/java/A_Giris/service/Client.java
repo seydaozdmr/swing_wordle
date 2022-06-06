3f2901bb8fa1173de111ca58f2750ccf3e2087b1
tableonthewall <seydaozdemir07@gmail.com>
2022-06-06 14:51:25 +0300

    adding labels and fixed over heap usage

--- src/main/java/A_Giris/service/Client.java
+++ src/main/java/A_Giris/service/Client.java
@@ -1,6 +1,8 @@
 package A_Giris.service;
 
 import javax.swing.*;
+import java.awt.*;
+import java.io.BufferedInputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -8,6 +10,7 @@ import java.net.Socket;
 
 public class Client extends JFrame {
     public static Socket socket;
+    private static JLabel [] myArray =new JLabel[25];
 
     public static void main(String[] args) {
         javax.swing.SwingUtilities.invokeLater(new Runnable() {
@@ -20,6 +23,7 @@ public class Client extends JFrame {
         try {
             socket=new Socket("localhost",1234);
             System.out.println("connected server");
+            getData();
         } catch (IOException e) {
             System.out.println("bağlantı kurulamadı");
             e.printStackTrace();
@@ -27,30 +31,72 @@ public class Client extends JFrame {
     }
 
     public Client(){
+        ControlService controlService=new ControlService();
         setTitle("Client");
         setSize(800 ,800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Point labelsStartPoint=new Point(200,10);
+        myArray = controlService.createLabelsForKeyBoard(this,labelsStartPoint);
 
         setLayout(null);
-        getData();
+        //getData();
     }
 
     private static void getData() {
-        JLabel[] result=new JLabel [25];
-        DataInputStream dIn = null;
-        try {
-            dIn = new DataInputStream(socket.getInputStream());
-            int length = dIn.readInt();                    // read length of incoming message
-            if(length>0) {
-                byte[] message = new byte[length];
-                dIn.readFully(message, 0, message.length);
-                System.out.println(message);// read the message
+        Thread t1=new Thread(new Runnable() {
+
+
+
+            @Override
+            public void run() {
+                DataInputStream dIn=null;
+                {
+                    try {
+                        dIn = new DataInputStream(socket.getInputStream());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                while (socket.isConnected()) {
+                    //int length = dIn.readInt();
+
+                    try {
+//                        DataInputStream dIn=null;
+//                        {
+//                            try {
+//                                dIn = new DataInputStream(socket.getInputStream());
+//                            } catch (IOException e) {
+//                                e.printStackTrace();
+//                            }
+//                        }
+                        byte[] message;
+                        int length = dIn.readInt();
+//                            if (length == 0)
+//                                break;
+                        System.out.println("mesaj uzunluğu: " + length);// read length of incoming message
+                        message = new byte[length];
+                        dIn.readFully(message);
+
+                        for (int i = 0; i < message.length; i++) {
+                            if(myArray[1]!=null && message[i]!=32){
+                                myArray[i].setText(String.valueOf((char)message[i]));
+                            }
+
+                            //System.out.println(String.valueOf(message[i]));
+                        }
+                        try {
+                            Thread.sleep(5000);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        });
 
-
+        t1.start();
     }
 
 
