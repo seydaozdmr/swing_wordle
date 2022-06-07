20e6a797a4f3a427157c0014d842236b7213a2bc
tableonthewall <seydaozdemir07@gmail.com>
2022-06-07 15:03:08 +0300

    word list added

--- src/main/java/A_Giris/model/WordList.java
+++ src/main/java/A_Giris/model/WordList.java
@@ -5,14 +5,52 @@ import java.util.List;
 public class WordList {
     public static List<String> words;
 
-
     public WordList() {
-
 
     }
 
     public static void createWords(){
-        words=  List.of("kalem","kağıt","durak","deniz","takip");
+        words=  List.of("kalem","kağıt","durak","deniz","takip","abacı","abdal","abiye","abone","acaba","acele","acılı",
+                "acıma","açgöz","açmak","açmaz","adana","afgan","afişe","afşar","afyon","agora","ağmak","ahbap","ahkam",
+                "ahlak","akıcı","akran","aksan","akşam","albay","alçak","alkış","alman","amele","andaç","antre","aptal",
+                "araba","arıcı","arşın","asabi","atari","avans","aymak","ayraç","Babai","babam","babaç","bacak","badal",
+                "badas","badat","badem","badik","badya","badıç","Bafra","bagaj","baget","Bahai","bahar","bahir","bahis",
+                "bahri","bahçe","bakır","balet","balon","balta","balık","bamya","banka","banko","banyo","baret","barış",
+                "basen","bavul","bayan","cacık","cadde","cahil","caize","calip","camcı","camia","camit","camlı","camsı",
+                "camız","canan","canik","canip","canlı","canım","carlı","carta","casus","cayma","damat","damga","damla",
+                "damlı","danca","danış","daraç","daraş","darbe","darca","dasit","datif","datça","davar","davet","davul",
+                "davya","dayak","dağar","dağcı","egzoz","ehram","ehven","ejder","ekici","ekili","eklem","ekler","ekmek",
+                "ekose","ekran","eksen","ekser","eksik","eksin","eksiz","eküri","elbet","elcik","elden","faraş","farba",
+                "faril","fariğ","faset","fasih","fasit","faska","faslı","fason","fasık","fasıl","fatih","fatsa","fauna",
+                "fayda","fazla","fazıl","fecir","fedai","garbi","garip","garoz","gasil","gauss","gavot","gayda","gayet",
+                "gayri","gayrı","gayur","gayya","gazal","gazap","gazel","gazla","gazlı","gazoz","gazve","gaşiy","haile",
+                "hakan","Hakas","hakem","hakim","hakir","haklı","hakça","halas","halat","halay","Halaç","halef","halel",
+                "halfa","Halik","halim","halis","haliç","halka","ızgın","ızrar","ığrıp","ışkın","ıştır","ışıma","ibare",
+                "ibate","ibdai","iblağ","iblis","ibraz","ibret","ibrik","ibzal","icbar","icmal","idadi","idame","idare",
+                "idari","iddia","ideal","idman","idrak","idrar","kabak","kaban","kabil","kabin","kabir","kablo","kabuk",
+                "kabul","kabza","kabız","kadar","kadeh","kadem","kader","kadim","kadir","kadit","kadro","kadük","kadın",
+                "laden","lades","ladin","lafzi","lafçı","lafız","lagar","lagos","lagün","lahit","lahos","lahut","lahza",
+                "lakap","lakin","lakoz","lakçı","lamba","lamel","lando","maada","mabat","mabet","mabut","macar","macun",
+                "madam","madde","maddi","madem","maden","mader","madik","madun","mafiş","mafya","magma","magri","mahal",
+                "mahfe","nabız","nacak","nadan","nadas","nadim","nadir","nafia","nafiz","nafta","nahak","nahif","nahiv",
+                "nahoş","nahır","nakdî","nakil","nakip","nakit","naklî","nakıs","oberj","obruk","ocuma","odacı","odalı",
+                "odeon","odsuz","ofans","oflaz","ofris","ofset","ojeli","okapi","okluk","oksit","oktan","oktav","okuma",
+                "okume","olası","öbürü","ödeme","ödlek","ödünç","öksüz","ölgün","ölmek","ölmez","ölmüş","ölçek","ölçer",
+                "ölçme","ölçüm","ölçün","ölçüt","ölçüş","öncel","öncül","önder","öneri","pabuç","padok","pafta","pagan",
+                "pahal","paket","palan","palas","palaz","palet","palto","pampa","pamuk","panda","panel","panik","papak",
+                "papaz","papel","paraf","sabah","saban","sabit","sabuh","sabun","sabur","sabık","sabır","sadak","sadet",
+                "sadik","sadme","sadık","sadır","safer","safha","safir","safra","safça","sahaf","şaban","şafak","şafii",
+                "şaful","şahap","şahin","şahit","şahne","şahsi","şahıs","şaibe","şaire","şakak","şakul","şalak","şaman",
+                "şamar","şamil","şanlı","şapel","tabak","taban","tabii","tabip","tabir","tabla","tablo","tabur","tabut",
+                "tabya","tacik","tacil","tacir","taciz","tadat","tadil","tadım","tafra","tafta","tahin","ucube","ukala",
+                "uknum","ulama","ulema","ulufe","uluma","umacı","ummak","umman","umumi","unluk","unsur","unvan","urban",
+                "urgan","usanç","usare","uskur","ussal","ücret","üdeba","üfleç","ülfet","ülger","ülker","ülser","ümera",
+                "ümmet","ümran","ündeş","ünite","ünlem","ünsüz","üreme","üremi","ürkek","ürkme","ürküş","üryan","vacip",
+                "vagon","vahim","vahit","vahiy","vahşi","vakar","vakfe","vakit","vakum","vakur","vakıa","vakıf","valiz",
+                "valör","vanlı","vapur","varak","varan","varda","yaban","yabgu","yafta","yahey","yahni","yahut","yahşi",
+                "yakin","yakma","yakut","yakım","yakın","yakıt","yakış","yalak","yalan","yalaz","yalpa","yalpı","yalım",
+                "zabit","zabıt","zafer","zahir","zahit","zalim","zaman","zamir","zamlı","zamme","zanka","zanlı","zarar",
+                "zarcı","zarif","zarsı","zarta","zaten","zayıf","zağar");
     }
 
 
