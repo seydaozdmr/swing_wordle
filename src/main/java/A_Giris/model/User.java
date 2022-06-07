bfa78afec31376d7538faa37a462b3d7b945ebb1
tableonthewall <seydaozdemir07@gmail.com>
2022-06-07 13:33:04 +0300

    added static fields

--- src/main/java/A_Giris/model/User.java
+++ src/main/java/A_Giris/model/User.java
@@ -10,10 +10,22 @@ public class User {
     private GameControls gameControls;
     //threadler aynı anda kullanırken senkronize olması açısından
     private AtomicBoolean isActive = new AtomicBoolean(false);
+    private boolean isWin;
+    private int playedCount;
+    private int wined;
+    private int loosed;
 
     public User(String userName, int score) {
         this.userName = userName;
         this.score = score;
+    }
+
+    public User(String userName, int score, int playedCount, int wined, int loosed) {
+        this.userName = userName;
+        this.score = score;
+        this.playedCount = playedCount;
+        this.wined = wined;
+        this.loosed = loosed;
     }
 
     public User() {
@@ -49,5 +61,37 @@ public class User {
 
     public void setGameControls(GameControls gameControls) {
         this.gameControls = gameControls;
+    }
+
+    public int getPlayedCount() {
+        return playedCount;
+    }
+
+    public void setPlayedCount(int playedCount) {
+        this.playedCount = playedCount;
+    }
+
+    public int getWined() {
+        return wined;
+    }
+
+    public void setWined(int wined) {
+        this.wined = wined;
+    }
+
+    public int getLoosed() {
+        return loosed;
+    }
+
+    public void setLoosed(int loosed) {
+        this.loosed = loosed;
+    }
+
+    public boolean isWin() {
+        return isWin;
+    }
+
+    public void setWin(boolean win) {
+        isWin = win;
     }
 }
